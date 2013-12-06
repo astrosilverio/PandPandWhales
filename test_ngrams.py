076447ef21a93d66bef97945bdc81369e0e3fb0c
Katie Silverio <astrosilverio@gmail.com>
2013-12-06 18:15:36 -0500

    more TDD

--- test_ngrams.py
+++ test_ngrams.py
@@ -7,7 +7,7 @@ from collections import defaultdict
 class Test_NGrams(unittest.TestCase):
 
     def setUp(self):
-        self.test_text = "I have bought this bonnet.\r\nI am Mr. Darcy.\r\nI jest.\r\n"
+        self.test_text = "I have bought this bonnet.\r\nI am Mr. Darcy.\r\nI am.\r\n"
         with open("in.txt",'w') as f:
             f.write(self.test_text)
         self.test_outfile = "out.txt"
@@ -27,8 +27,19 @@ class Test_NGrams(unittest.TestCase):
     
     def test_bigrams_counter(self):
         self.assertEqual(type(self.test_markov.bigram_freqs), defaultdict)
-        self.assertEqual(self.test_markov.bigram_freqs["I"]["am"], 1)
+        self.assertEqual(self.test_markov.bigram_freqs["I"]["am"], 2)
         self.assertEqual(self.test_markov.bigram_freqs["."]["**End**"], 3)
+        
+    def test_trigrams(self):
+        self.assertEqual(type(self.test_markov.trigrams), defaultdict)
+        self.assertEqual(round(sum(self.test_markov.trigrams[("I","am")].values()), 5), 1.0)
+        self.assertEqual(self.test_markov.trigrams[("**Beginning**","I")]["am"], 2/3.)
+        self.assertEqual(self.test_markov.trigrams[("I","have")]["rocketships"], 1e-5)
+
+    def test_trigrams_counter(self):
+        self.assertEqual(type(self.test_markov.trigram_freqs), defaultdict)
+        self.assertEqual(self.test_markov.trigram_freqs[("Darcy",".")]["**End**"], 1)
+        self.assertEqual(self.test_markov.trigram_freqs[("**Beginning**","I")]["am"], 2)
     
     def tearDown(self):
         os.remove("in.txt")
